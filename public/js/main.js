d349651470a1db2748e3d27a848a1690f7b1cdf2
alexreu <alexxxx.974@gmail.com>
2018-06-07 15:46:29 +0400

    ajout de la couleur des etoiles

--- public/js/main.js
+++ public/js/main.js
@@ -1,42 +1,58 @@
-var etoile = function(nbStars){
+function etoile(nbStars){
     stars = '<i class="fas fa-star"></i>';
+    switch (nbStars) {
+        case 1:
+        case 2:
+            stars = '<i class="fas fa-star rouge"></i>'
+            break;
+        case 3:
+        case 4:
+            stars = '<i class="fas fa-star vert-clair"></i>'
+            break;
+        default:
+            stars = '<i class="fas fa-star vert"></i>'
+            break;
+    }
     noStars = '<i class="far fa-star"></i>';
+    switch (nbStars) {
+        case 1:
+        case 2:
+            noStars = '<i class="far fa-star rouge"></i>'
+            break;
+        case 3:
+        case 4:
+            noStars = '<i class="far fa-star vert-clair"></i>'
+            break;
+        default:
+            noStars = '<i class="far fa-star vert"></i>'
+            break;
+    }
 
     var result ="";
     var rest = 0;
+
     reste = 6 - nbStars;
     result = stars.repeat(nbStars) + noStars.repeat(reste);
     
     return result;
 }
+
+
 $.ajax({
     url: "http://localhost:3012/public/data/data.json",
     success : function(data){
 
         var tab = [];
-        var stars = '<i class="fas fa-star"></i>';
-        var noStars = '<i class="far fa-star"></i>';
-
-        console.log(data[0].competence.niveau);
 
         data.sort(function(a, b){
             return b.competence.niveau - a.competence.niveau;
         });
         
-        for (var id in data){
-            var tmp = '<li>'+ data[id].competence.competence_name + " " + etoile(data[id].competence.niveau) + '</li>';
-            // etoile(data[id].competence.niveau)
+        for (var i=0; i<data.length; i++){
+            var tmp = '<li class="list-group-item text-uppercase">'+ data[i].competence.competence_name + " " + etoile(data[i].competence.niveau) + '</li>';
             tab.push(tmp);
         }
         
         $("#liste").html(tab);
-
     }
 });
-
-
-
-// ma fonction(4){
-//  etoile = <i>etoile</i>
-
-//  retourn : 4*etoile + 4-6noetoile
