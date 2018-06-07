9cac423170d6bfeb9d6d5cd4b1d4efcfa0fc818d
alexreu <alexxxx.974@gmail.com>
2018-06-07 13:38:52 +0400

    trie data ok + affichage liste ok

--- public/js/main.js
+++ public/js/main.js
@@ -2,23 +2,21 @@ $.ajax({
     url: "http://localhost:3012/public/data/data.json",
     success : function(data){
 
-        var  tab = [];
-        var competence = [];
+        var tab = [];
         var stars = '<i class="fas fa-star"></i>';
         var noStars = '<i class="far fa-star"></i>';
 
         console.log(data[0].competence.niveau);
 
+        data.sort(function(a, b){
+            return b.competence.niveau - a.competence.niveau;
+        });
         
         for (var id in data){
-            var tmp ='<li>' + data[id].competence.niveau + '</li>';
+            var tmp = '<li>'+ data[id].competence.competence_name + " " + stars.repeat(3)+ noStars.repeat(2)+ " " + data[id].competence.niveau + '</li>';
             tab.push(tmp);
         }
-
-        console.log(tab);
-
-        tab.sort()
-
+        
         $("#liste").html(tab);
     }
 })
